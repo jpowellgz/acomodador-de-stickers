21a212bf0dc0057944ca9bb837130336d95962ca
jpowellgz <jaipogo@hotmail.com>
2026-06-04 23:37:56 -0600

    ok

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-import cv2
-from src.acomodador_de_stickers.acomodador import Acomodador, Imagen, mostrar_imagen
 from src.acomodador_de_stickers.interfaz import crear_interfaz
 
 def main():
-    acomodador = Acomodador(10, 8, [0.5, 0.5, 0.5, 0.5])
-    #acomodador.mostrar_plantilla()
-    # imagen = Imagen("imagen_de_prueba.png")
-    #acomodador.acomodar_rectangular(imagen, "alto", 3)
-    # acomodador.acomodar_circular(imagen, "ancho", 5)
-    # acomodador.mostrar_plantilla()
     crear_interfaz()
-
-
 
 if __name__ == "__main__":
     main()
